2587fb329c2bc5282927069cb7477b6b8ad04edf
Jorge A. Gomes <jorgegomes83@hotmail.com>
2015-03-23 23:06:54 -0300

    Module added.

--- spacegame/__init__.py
+++ spacegame/__init__.py
@@ -6,7 +6,10 @@ A simple 2d space shooter made with python and pygame.
 # Make sure you ha python34 and pygame 1.9.1+ installed before run this code.
 import pygame
 import pygame.locals as c
+from .core import Game
 
 # this module itself does nothing important, but its good to have pygame
 # initialized as soon as possible.
 pygame.init()
+
+Game.run()
