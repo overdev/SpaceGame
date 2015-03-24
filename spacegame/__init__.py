16e6f88e094d4eac8ba154eed5681187f14ab652
Jorge A. Gomes <jorgegomes83@hotmail.com>
2015-03-23 21:39:03 -0300

    Create __init__.py
    This file is the game launcher.

---
+++ spacegame/__init__.py
@@ -0,0 +1,12 @@
+"""SpaceGame
+
+A simple 2d space shooter made with python and pygame.
+"""
+
+# Make sure you ha python34 and pygame 1.9.1+ installed before run this code.
+import pygame
+import pygame.locals as c
+
+# this module itself does nothing important, but its good to have pygame
+# initialized as soon as possible.
+pygame.init()
